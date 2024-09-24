b310b4b6eca0ae9d7a8f73d2ce08d95b47b6c4c1
flami <flamiartist@foxmail.com>
2024-09-24 13:33:00 +0800

    Added new scene
    Added new scene: level_moca_0924

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 @export var player : CharacterBody2D = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
